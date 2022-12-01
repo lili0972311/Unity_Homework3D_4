9ef446d92faafea999836f2ca4aa665c7607cf89
陳昕儀 <113577945+lili0972311@users.noreply.github.com>
2022-12-01 15:55:14 +0800

    Step 02
    很多

---
+++ Unity_Homework3D_4/Assets/script/DialogueData.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+namespace KUMO
+{
+    /// <summary>
+    /// 對話資料
+    /// </summary>
+    [CreateAssetMenu(menuName = "KUMO/Dialogue Data", fileName = "New Dialogue Data")]
+    public class DialogueData : ScriptableObject
+    {
+        [Header("對話者名稱")]
+        public string dialogueName;
+        [Header("對話者內容"),TextArea(2,10)]
+        public string[] dialogueContents;
+    }
+}
---
+++ Unity_Homework3D_4/Assets/script/DialogueSystem.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace KUMO
+{
+    /// <summary>
+    /// 對話系統
+    /// </summary>
+
+    public class DialogueSystem : MonoBehaviour
+    {
+        [SerializeField, Header("對話間隔"), Range(0, 0.5f)]
+        private float dialogueIntervalTime = 0.1f;
+        [SerializeField, Header("開頭對話")]
+        private DialogueData dialogueOpening;
+
+        private WaitForSeconds dialogueInterval => new WaitForSeconds(dialogueIntervalTime);
+
+        private CanvasGroup groupDialogue;
+        private TextMeshProUGUI textName;
+        private TextMeshProUGUI textContent;
+        private GameObject goTriangle;
+
+        private void Awake()
+        {
+            groupDialogue = GameObject.Find("畫布對話系統").GetComponent<CanvasGroup>();
+            textName = GameObject.Find("對話者名稱").GetComponent<TextMeshProUGUI>();
+            textContent = GameObject.Find("對話者內容").GetComponent<TextMeshProUGUI>();
+            goTriangle = GameObject.Find("對話完成圖示");
+            goTriangle.SetActive(false);
+        }
+    }
+}
