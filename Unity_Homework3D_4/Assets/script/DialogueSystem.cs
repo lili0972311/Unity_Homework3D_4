7472a3ef116070fad04dda56576dcb3ea8f7ba29
陳昕儀 <113577945+lili0972311@users.noreply.github.com>
2022-12-08 15:56:46 +0800

    Step03
    很多

--- Unity_Homework3D_4/Assets/script/DialogueSystem.cs
+++ Unity_Homework3D_4/Assets/script/DialogueSystem.cs
@@ -1,5 +1,6 @@
 ﻿using TMPro;
 using UnityEngine;
+using System.Collections;
 
 namespace KUMO
 {
@@ -28,6 +29,35 @@ namespace KUMO
             textContent = GameObject.Find("對話者內容").GetComponent<TextMeshProUGUI>();
             goTriangle = GameObject.Find("對話完成圖示");
             goTriangle.SetActive(false);
+
+            StartCoroutine(FadeGroup());
+            StartCoroutine(TypeEffect());
+
+        }
+
+        private IEnumerator FadeGroup()
+        {
+            for (int i =0; i< 10; i++)
+            {
+                groupDialogue.alpha += 0.1f;
+                yield return new WaitForSeconds(0.04f);
+            }
+        }
+
+        private IEnumerator TypeEffect()
+        {
+            textName.text = dialogueOpening.dialogueName;
+            textContent.text = "";
+
+            string dialogue = dialogueOpening.dialogueContents[1];
+
+            for (int i = 0; i < dialogue.Length; i++)
+            {
+                textContent.text += dialogue[i];
+                yield return dialogueInterval;
+            }
+
+            goTriangle.SetActive(true);
         }
     }
 }
---
+++ Unity_Homework3D_4/Assets/script/LearnCoroutine.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+namespace KUMO
+{ 
+    /// <summary>
+    /// 學習偕同程式，簡稱協程 Coroutine
+    /// 目的:讓程式達到停留的效果
+    /// </summary>
+public class LearnCoroutine : MonoBehaviour
+{
+        //使用偕同程式的四個條件
+        //1.引用命名空間 System.Collections
+        //2.定義一個傳回 IEnumerator
+        //3.方法內必須使用 yield return
+        //4.使用 StartCoroutine 啟動
+
+        //字串 string 為 char 陣列
+        private string testDialogue = "我在哪裡?";
+
+        private void Awake()
+        {
+            //StartCoroutine(Test());
+
+            //print("取得測試對話的第一個字:" + testDialogue[0]);
+
+            //StartCoroutine(ShowDialogue());
+            StartCoroutine(ShowDialogueUseFor());
+        }
+
+        private IEnumerator Test()
+        {
+            print("<color= #33ff33>第一行程式</Color>");
+            yield return new WaitForSeconds(2);
+            print("<color=#ff3333>第二行程式</color>");
+            yield return new WaitForSeconds(3);
+            print("<color=#ff3333>第三行程式</color>");
+
+        }
+
+        private IEnumerator ShowDialogue()
+        {
+            print(testDialogue[0]);
+            yield return new WaitForSeconds(0.1f);
+            print(testDialogue[1]);
+            yield return new WaitForSeconds(0.1f);
+            print(testDialogue[2]);
+            yield return new WaitForSeconds(0.1f);
+
+
+        }
+
+        private IEnumerator ShowDialogueUseFor()
+        {
+            for (int i = 0; i < testDialogue.Length; i++)
+            {
+                print(testDialogue[i]);
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+
+
+    }
+
+}
